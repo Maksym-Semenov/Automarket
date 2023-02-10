6dc20a6d96de73b287ab10af9ca42b2239d9f7b4
DeathForRussia <ramirkiev@gmail.com>
2023-02-10 14:57:48 +0200

    testing app

---
+++ Automarket/Controllers/CarController.cs
@@ -0,0 +1,20 @@
+﻿using Automarket.DAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Automarket.Controllers;
+
+public class CarController : Controller
+{
+    private readonly ICarRepository _carRepository;
+
+    public CarController(ICarRepository carRepository)
+    {
+        _carRepository = carRepository;
+    }
+    // GET
+    public async Task<IActionResult> GetCars()
+    {
+        var responce = await _carRepository.Select();
+        return View(responce);
+    }
+}
